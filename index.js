7fa615ff012c6b9ef53077316d0dad34ecca1614
Hemapriya182024 <hemakarthikeyan2024@gmail.com>
2024-11-30 15:06:57 +0530

    updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app=express();
 const PORT=process.env.PORT
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://project-umber-delta.vercel.app/',
     credentials: true
 }));
 connectDB()
